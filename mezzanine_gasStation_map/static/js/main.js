25f3cb03f662fc7c0a9531c2943e5853b4518177
GustavoAlfredoZA <GustavoZA20@hotmail.com>
2020-06-11 17:09:04 -0500

    map

--- mezzanine_gasStation_map/static/js/main.js
+++ mezzanine_gasStation_map/static/js/main.js
@@ -2,8 +2,7 @@ var mymap = L.map('mapid').setView([19.6493721,-101.2209878], 13);
 
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
   maxZoom: 18,
-  attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
-  'Gustavo Alfredo Zárate Acosta Antonio Chacon Flores, ' +
+  attribution:'Gustavo Alfredo Zarate Acosta Antonio Chacon Flores, ' +
   'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
   id: 'mapbox/streets-v11',
   tileSize: 512,
